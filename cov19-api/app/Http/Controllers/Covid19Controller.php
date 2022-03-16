94f8d3a9fe34ba12213a76cfd48e7ee5863021b7
stevemooney123 <stephen@kaizenappz.com>
2022-03-16 09:20:13 +0000

    controller
    add first controller and route

---
+++ cov19-api/app/Http/Controllers/Covid19Controller.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Covid19Controller extends Controller
+{
+    //
+    public function cases(){
+        return "cases";
+    }
+}
