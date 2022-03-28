22ddb53eda700c044fcec9b926ca3e4b50add271
stevemooney123 <stephen@kaizenappz.com>
2022-03-28 09:31:23 +0100

    create dashboard array of objects

--- cov19-api/app/Http/Services/Cov19Service.php
+++ cov19-api/app/Http/Services/Cov19Service.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use stdClass;
 use \YaLinqo\Enumerable;
+
 class Cov19Service
 {
     public function GetMetrics(Request $request)
@@ -18,23 +19,51 @@ class Cov19Service
 
     public function GetLast7Days(Request $request)
     {
-        $object = new stdClass();
+        $stack = array();
+
+        $object1 = new stdClass();
+        $object2 = new stdClass();
 
         $days = $request->get("days");
 
-        $casesLast7Days = from($this->extracted($request))->select(function($np){ return $np['newCases']; })->take($days)->sum($request['newCases']);
-        $dailyCases = from($this->extracted($request))->select(function($np){ return $np['dailyDeaths']; })->take(1)->sum($request['dailyDeaths']);
-        $totalCases = from($this->extracted($request))->select(function($np){ return $np['cumulativeCases']; })->take(1)->sum($request['cumulativeCases']);
+        $casesLast7Days = from($this->extracted($request))->select(function ($np) {
+            return $np['newCases'];
+        })->take(7)->sum($request['newCases']);
+        $dailyCases = from($this->extracted($request))->select(function ($np) {
+            return $np['newCases'];
+        })->take(1)->sum($request['newCases']);
+        $totalCases = from($this->extracted($request))->select(function ($np) {
+            return $np['cumulativeCases'];
+        })->take(1)->sum($request['cumulativeCases']);
 
         $casesLast7DaysName = "Cases last 7 days";
         $dailyCasesName = "Daily Cases";
         $totalCasesName = "Total Cases";
-        $object->$casesLast7DaysName =$casesLast7Days;
-        $object->$dailyCasesName =$dailyCases;
-        $object->$totalCasesName =$totalCases;
+
+        $object1->$dailyCasesName = $dailyCases;
+        $object1->$casesLast7DaysName = $casesLast7Days;
+        $object1->$totalCasesName = $totalCases;
 
 
-        return $object;
+        $deathsLast7Days = from($this->extracted($request))->select(function ($np) {
+            return $np['dailyDeaths'];
+        })->take(7)->sum($request['dailyDeaths']);
+        $deathCases = from($this->extracted($request))->select(function ($np) {
+            return $np['dailyDeaths'];
+        })->take(1)->sum($request['dailyDeaths']);
+        $totalDeaths = from($this->extracted($request))->select(function ($np) {
+            return $np['cumulativeDeaths'];
+        })->take(1)->sum($request['cumulativeDeaths']);
+
+        $deathsLast7DaysName = "Deaths last 7 days";
+        $dailyDeathsName = "Daily Deaths";
+        $totalDeathsName = "Total Deaths";
+
+        $object2->$deathsLast7DaysName = $deathsLast7Days;
+        $object2->$dailyDeathsName = $deathCases;
+        $object2->$totalDeathsName = $totalDeaths;
+        $arr = array($object1, $object2);
+        return $arr;
 
     }
 
